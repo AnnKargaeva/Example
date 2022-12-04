470c06cbd5518a0a6c390de9dc82774b5953d6dc
Anna <annet0924@yandex.ru>
2022-12-05 00:06:54 +0300

    Change 008.02

---
+++ Example008.002_introMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿int Max(int arq1, int arq2, int arq3)
+{
+    int result = arq1;
+    if(arq2 > result) result = arq2;
+    if(arq3 > result) result = arq3;
+    return result;
+}
+//              0 1 2 3 4 5 6 7 8 
+int [] array = {11, 21, 31, 41, 15, 61, 17, 18, 19};
+array[0] = 12;
+Console.WriteLine(array[4]);
+
