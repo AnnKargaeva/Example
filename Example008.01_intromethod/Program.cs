da7d8595160c6d2b05a65088ad429c65627ac2b6
Anna <annet0924@yandex.ru>
2022-12-04 23:40:30 +0300

    Add 008.01

--- Example008.01_intromethod/Program.cs
+++ Example008.01_intromethod/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max(int arq1, int arq2, int arq3)
+{
+    int result = arq1;
+    if(arq2 > result) result = arq2;
+    if(arq3 > result) result = arq3;
+    return result;
+}
+int a1 = 15;
+int b1 = 21;
+int c1 = 39;
+int a2 = 15;
+int b2 = 23;
+int c2 = 33;
+int a3 = 13;
+int b3 = 23;
+int c3 = 313;
+
+int max1  = Max(a1, b1, c1);
+int max2  = Max(a2, b2, c2);
+int max3  = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);
+
+Console.WriteLine(max);
